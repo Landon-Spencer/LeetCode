f7ec89657aae8e3b9ac4d14fb81e3bf7e43c98ae
Landon-Spencer <landon.r.spencer@gmail.com>
2026-09-12 12:07:39 -0600

    updated merge

--- MergeTwoSortedList.py
+++ MergeTwoSortedList.py
@@ -10,3 +10,37 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
+        if len(list2) > len(list1):
+            long = list2
+            short = list1
+        else:
+            long = list1
+            short= list2
+        shortCut = 0
+        res = []
+        for indexLong, itemLong in enumerate(long):
+            for indexShort, itemShort in enumerate(short):
+                if itemShort <= itemLong:
+                    res.append(itemShort)
+                    shortCut += 1
+                    if indexShort == len(short) - 1:
+                        res.extend(long[indexLong:])
+                        return res
+                else:
+                    res.append(itemLong)
+                    del short[:shortCut]
+                    shortCut = 0
+                    break
+        return res
+
+example = Solution()
+print(example.mergeTwoLists([1,2,4,5], [1,3,4]))
+print(example.mergeTwoLists([1,2,4], [1,3,4,5]))
+print(example.mergeTwoLists([1,2,4], [1,3,4]))
+print(example.mergeTwoLists([], []))
+print(example.mergeTwoLists([], [0]))
+print(example.mergeTwoLists([0], []))
